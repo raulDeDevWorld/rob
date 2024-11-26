c833334030439bd04208f1da2cc4422d0458a9f5
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2024-11-26 04:02:38 -0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -129,7 +129,7 @@ export default function Home() {
 
         <div className="max-w-7xl mx-auto">
           <h2 className="text-3xl font-bold text-gray-900 mb-8 text-left">Nuestro modo de trabajo</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4   gap-8">
+          <div className="md:grid md:grid-cols-2 lg:grid-cols-4   gap-8">
             <div className="shadow border p-5 col-span-2">
               <h3 className="text-[20px] text-black font-medium">Plataformas interactivas</h3>
               <p className="text-gray-600 pb-5">Todas las sesiones de nuestros cursos en linea se llevan a cabo en nuestra plataformas interactiva</p>
